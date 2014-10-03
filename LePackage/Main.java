986873135b08c0f1fc8ed48fc81e2ffca632eacf
Clément Sébillet <clement.sebillet@bts-malraux.net>
2014-10-03 11:56:30 +0200

    maj

--- LePackage/Main.java
+++ LePackage/Main.java
@@ -22,58 +22,61 @@ import LePackage.Calcul;
 
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
-
-		try{
-    		Class.forName("org.postgresql.Driver");
-    	} catch (Exception e) {
-    		System.out.println("Where is your PostgreSQL JDBC Driver? "
-					+ "Include in your library path!");
-			e.printStackTrace();
-    	}//Fin catch
-
-		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:postgresql://172..16.99.2:5432/csebillet","c.sebillet" ,"passe");
-
-
-		Statement maRequete = connection.createStatement();
-		String texteRequete = "select … from … where … ";
-
-		// définition de l'objet qui récupérera le résultat de l'exécution de la requête :
-
-		ResultSet curseurResultat = maRequete.executeQuery(texteRequete);
-
-		// récupération des détails du résultats
-
-		ResultSetMetaData detailsDonnees = curseurResultat.getMetaData();
+	public static void main(String[] args)  {
 
 		// crÃ©ation d'une liste
 		List<AR> maListeAR =  new ArrayList<AR>();
 		List<AS> maListeAS =  new ArrayList<AS>();
 
-		String monFichierTarif = "/home/etudiant/SLAMJAVA/RemboursementV2/src/LePackage/tarif.txt";
-
 		try{
-    		//Ouverture d'un flux d'entrée à partir du fichier "docTarif.txt"
-        	BufferedReader monBuffer = new BufferedReader(new FileReader(monFichierTarif));
-        	String line = null;					//Variable qui contiendra chaque ligne du fichier
-
-        	//Tant qu'il reste une ligne au fichier
-        	while ((line = monBuffer.readLine()) != null)
-        	{
-        		//On découpe la ligne gràce au caractère ","
-        		String[] part = line.split(",");
-        		//On ajoute un objet de la classe Tarif à la brochure, à partir de la ligne du fichier découpée
-        		maListeAR.add(new AR(Integer.parseInt(part[0]),Double.parseDouble(part[1]),Double.parseDouble(part[4]),Double.parseDouble(part[7]),
-        							Double.parseDouble(part[2]),Double.parseDouble(part[5])));
-        		maListeAS.add(new AS(Integer.parseInt(part[0]),Double.parseDouble(part[1]),Double.parseDouble(part[5]),Double.parseDouble(part[7]),
-						Double.parseDouble(part[3]),Double.parseDouble(part[6])));
-        	}
-        	//Fermeture du buffer
-        	monBuffer.close();
+			Class.forName("org.postgresql.Driver");
 		} catch (Exception e) {
-		    System.out.println("Fichier contenant les tarifs introuvable !!!");
-		  }
+		    System.out.println("Driver PostgreSQL introuvable !!!");
+		    System.exit(0);
+		}
+
+		//Création d'un objet de type Connection
+    	Connection maConnect = null;
+
+    	try{
+    		String url = "jdbc:postgresql://localhost:5432/csebillet";
+    		maConnect = DriverManager.getConnection(url, "c.sebillet", "passe");
+    	}catch(Exception e){
+    		System.out.println("Une erreur est survenue lors de la connexion à la base de donnée");
+    	}
+
+    	Statement maReq = null;
+
+    	try{
+    		maReq = maConnect.createStatement();
+    	}catch(Exception e){
+
+    	}
+
+    	String texteRequete = "select * from \"taxi\".tarif";
+
+		// définition de l'objet qui récupérera le résultat de l'exécution de la requête :
+		ResultSet curseurResultat = null;
+		try {
+			curseurResultat = maReq.executeQuery(texteRequete);
+		} catch (SQLException e) {
+
+		}
+
+		// récupération des détails du résultats
+		try {
+			ResultSetMetaData detailsDonnees = curseurResultat.getMetaData();
+			// tant qu'il y a encore une ligne résultat à lire
+			while(curseurResultat.next()){
+				maListeAR.add(new AR(curseurResultat.getInt(0), curseurResultat.getDouble(1) ,Double.parseDouble(part[4]),Double.parseDouble(part[7]),
+						Double.parseDouble(part[2]),Double.parseDouble(part[5])));
+				maListeAS.add(new AS(Integer.parseInt(part[0]),Double.parseDouble(part[1]),Double.parseDouble(part[5]),Double.parseDouble(part[7]),
+						Double.parseDouble(part[3]),Double.parseDouble(part[6])));
+			 }
+		} catch (SQLException e) {
+
+		}
+
 
 		int i;
 		/*
